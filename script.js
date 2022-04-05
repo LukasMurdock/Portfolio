48f422a1a088198bfdfe21ed1020b11d786544b2
Shay Jin <shin.810@osu.edu>
2022-04-05 03:37:32 -0400

    no JS! houyu: ?

--- script.js
+++
@@ -1,7 +0,0 @@
-function displayTextWidth(text, font) {
-    let canvas = displayTextWidth.canvas || (displayTextWidth.canvas = document.createElement("canvas"));
-    let context = canvas.getContext("2d");
-    context.font = font;
-    let metrics = context.measureText(text);
-    return metrics.width;
-  }
