6106a016eb138dea2c2e21a48e07738e9a44dcbc
Jay Shin <shin.810@osu.edu>
2022-08-29 18:31:17 -0400

    수업 추가ㅋ

--- script.js
+++ script.js
@@ -4,23 +4,26 @@ var myJobs = ["Software Engineer", "Full-Stack Web Developer", "iOS App Develope
 var myPics = ["shay.png", "shay2.png", "shay3.png", "shay4.png"];
 var bubbleMessages = ["GitHub", "LinkedIn", "Instagram", "SoundCloud"];
 var courseList = {
-    "Coding": 
+    "Computer Science": 
     [
+    "CSE 3231: Software Engineering Techniques",
+    "CSE 3461: Computer Networking and Internet Technologies",
+    "CSE 3241: Database Systems",
     "CSE 3901: Design, Development, and Documentation of Web Applications",
-    "CSE 2421: Low-Level Programming and Computer Organization",
+    "CSE 2331: Data Structures and Algorithms",
+    "CSE 2321: Discrete Mathematics and Structures",
     "CSE 2231: Software Development and Design",
-    "CSE 2221: Software Components"], 
-    "Logic": 
-    ["CSE 2331: Data Structures and Algorithms",
-    "CSE 2321: Discrete Mathematics and Structures"],
-    "Math":
-    ["STAT 3470: Probability and Statistics for Engineers",
-    "MATH 1172: Calculus 2 & 3"],
-    "Hardware": 
-    ["CSE 2431: Operating Systems",
+    "CSE 2221: Software Components",
+    "Advanced Placement Computer Science A"], 
+    "Computer Engineering": 
+    ["MATH 2568: Linear Algebra",
+    "CSE 2431: Operating Systems",
+    "CSE 2421: Low-Level Programming and Computer Organization",
     "ECE 2060: Digital Logic",
+    "STAT 3470: Probability and Statistics for Engineers",
     "ENGR 1282H: Fundamentals of Engineering for Honors II",
-    "ENGR 1281H: Fundamentals of Engineering for Honors I", ]
+    "ENGR 1281H: Fundamentals of Engineering for Honors I",
+    "MATH 1172: Engineering Calculus II" ]
 };
 
 myApp.controller('mainController', ['$scope', '$interval',
